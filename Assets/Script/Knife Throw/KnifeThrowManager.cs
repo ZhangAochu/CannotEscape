a55a364ccc5523c86002e9b4ab9f2cb2ac06be48
Zhang Aochu <acworld2004@163.com>
2025-03-26 07:18:06 +0800

    完成功能：1.基础场景转换，包括Sickroom，Sickroom Components，Corridor，Corridor Knife Throw等场景转换。\n2.飞刀扎人游戏大致功能完成：三次击杀击败boss，跳出胜利面板及返回键，血条显示功能完成。

--- Assets/Script/Knife Throw/KnifeThrowManager.cs
+++ Assets/Script/Knife Throw/KnifeThrowManager.cs
@@ -9,16 +9,18 @@ public class KnifeThrowManager : MonoBehaviour
     public static KnifeThrowManager Instance;
 
     [Header("UI Settings")]
-    //public Slider healthSlider;        // 血条滑动条组件
-    //public GameObject victoryPanel;    // 胜利提示面板
+    public Slider healthSlider;        // 血条滑动条组件
+    public GameObject victoryPanel;    // 胜利提示面板
 
     [Header("Game Settings")]
     public GameObject monsterPrefab;   // 怪物预制体
-    public Transform spawnPoint;       // 怪物生成位置
+    public Transform spawnPoint; 
+    public GameObject teleportToCorridor;
+    public GameObject backButton;      // 怪物生成位置
 
     private int maxHealth = 3;         // 怪物总血量（分3次击破）
     private int currentHealth;         // 当前剩余血量
-
+    
 
     // 在对象初始化时调用（早于Start）
     void Awake()
@@ -31,17 +33,19 @@ public class KnifeThrowManager : MonoBehaviour
     void InitializeGame()
     {
         currentHealth = maxHealth;                     // 重置血量
-        //healthSlider.maxValue = maxHealth;             // 设置血条最大值
-        //healthSlider.value = currentHealth;            // 同步当前血量显示
+        healthSlider.maxValue = maxHealth;             // 设置血条最大值
+        healthSlider.value = currentHealth;            // 同步当前血量显示
         SpawnMonster();                                // 生成第一个怪物
-        //victoryPanel.SetActive(false);                 // 隐藏胜利面板
+        victoryPanel.SetActive(false);                 // 隐藏胜利面板
+        teleportToCorridor.SetActive(false);
+        backButton.SetActive(false);
     }
 
     // 处理命中怪物事件
     public void HitMonster()
     {
         currentHealth--;                               // 扣除血量
-        //healthSlider.value = currentHealth;            // 更新血条显示
+        healthSlider.value = currentHealth;            // 更新血条显示
 
         if (currentHealth <= 0)
         {
@@ -61,7 +65,9 @@ public class KnifeThrowManager : MonoBehaviour
     // 显示胜利界面
     void ShowVictory()
     {
-        //victoryPanel.SetActive(true);                  // 激活胜利面板
+        victoryPanel.SetActive(true);                  // 激活胜利面板
         Time.timeScale = 0;                            // 暂停游戏时间
+        teleportToCorridor.SetActive(true);
+        backButton.SetActive(true);
     }
 }
